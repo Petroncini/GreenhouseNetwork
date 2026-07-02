79a9edbdf4f8e7f9403cb51d0af37f548f0dba9d
Caio Petroncini <59212480+Petroncini@users.noreply.github.com>
2026-07-02 15:52:40 -0300

    Feature/control thread (#1)
    * feat: mutex in state variables
    
    * fix: split mutex
    
    * feat: control implemented
    
    ---------
    
    Co-authored-by: Nickson8 <nicolasdesousamaia@gmail.com>
    Co-authored-by: Nickson8 <162637109+Nickson8@users.noreply.github.com>

--- manager.cpp
+++ manager.cpp
@@ -6,6 +6,7 @@
 #include <thread>
 #include <unistd.h>
 #include <unordered_map>
+#include <mutex>
 
 using namespace std;
 
@@ -32,13 +33,31 @@ public:
   int udp_socket;
   bool sensor_registered;
 
+  //Variaveis de controle do manager
+  float max_temp;
+  float min_temp;
+  float max_co2;
+  float min_co2;
+  float max_humidity;
+  float min_humidity;
+
   unordered_map<int, DeviceInfo> sensors;
   unordered_map<int, DeviceInfo> actuators;
+
+  mutex state_mutex;
+  mutex device_mutex;
 
   Manager() {
     temp = 0;
     co2 = 0;
     humidity = 0;
+
+    max_temp = 9999999;
+    min_temp = -1;
+    max_co2 = 9999999;
+    min_co2 = -1;
+    max_humidity = 9999999;
+    min_humidity = -1;
     sensor_registered = false;
   }
 
@@ -134,6 +153,7 @@ public:
   }
 
   void register_device(const DeviceInfo &device, DeviceClass cls) {
+    lock_guard<mutex> lock(device_mutex);
     if (cls == DEVICE_CLASS_SENSOR) {
       sensors[device.id] = device;
       if(!sensor_registered) {
@@ -177,8 +197,23 @@ public:
         perror("send");
       }
 
+  void handle_sensor_data(DeviceType type, float data) {
+    lock_guard<mutex> lock(state_mutex);
+    switch (type) {
+      case (DEVICE_TEMP_SENSOR_OR_HEATER):
+        temp = data;
+        break;
+      case (DEVICE_SOIL_MOISTURE_OR_IRRIGATION):
+        humidity = data;
+        break;
+      case (DEVICE_CO2_SENSOR_OR_INJECTOR):
+        co2 = data;
+        break;
+      default:
+        break;
       std::thread(&Manager::connection_handler, this, device).detach();
     }
+    //Destrava automaticamente o mutex
   }
 
 
@@ -199,6 +234,7 @@ public:
         ssize_t bytes_read = recv(device.socket, &msg, sizeof(msg), MSG_WAITALL);
         if (bytes_read == sizeof(msg)) {
           // Atualiza o estado do atuador na memória do servidor
+          lock_guard<mutex> lock(device_mutex);
           actuators[msg.id].status = msg.status;
           std::cout << "Actuator status received: " << (int)msg.id << " -> " 
                     << (msg.status == ACTUATOR_ON ? "ON" : "OFF") << std::endl;
@@ -216,6 +252,54 @@ public:
     }
   }
 
+  void find_set_actuators(DeviceType tipo, ActuatorStatus status){
+    lock_guard<mutex> lock(device_mutex);
+    for(auto &[id, device] : actuators){
+      if(device.type == tipo && device.status != status){
+        ActuatorSet msg;
+        msg.header.first_byte = (PROTOCOL_ID << 4) | ACTUATOR_SET;
+        msg.id = device.id;
+        msg.status = status;
+        send(device.socket, &msg, sizeof(msg), 0);
+        device.status = status;
+      }
+    }
+  };
+
+  void control_loop(){
+    float local_temp, local_co2, local_humidity;
+    while(true){
+      //Checar se algum valor esta fora do padrao
+      
+      {
+        lock_guard<mutex> lock(state_mutex);
+        local_temp = temp;
+        local_co2 = co2;
+        local_humidity = humidity;
+      }
+      if(local_temp > max_temp){ //Desliga os aquecedores e liga os coolers
+        find_set_actuators(DEVICE_TEMP_SENSOR_OR_HEATER, ACTUATOR_OFF);
+        find_set_actuators(DEVICE_COOLER, ACTUATOR_ON);
+      }
+      if(local_temp < min_temp){ //Liga os aquecedores e desliga os coolers
+        find_set_actuators(DEVICE_TEMP_SENSOR_OR_HEATER, ACTUATOR_ON);
+        find_set_actuators(DEVICE_COOLER, ACTUATOR_OFF);
+      }
+      if(local_co2 > max_co2){ //Desliga os injetores
+        find_set_actuators(DEVICE_CO2_SENSOR_OR_INJECTOR, ACTUATOR_OFF);
+      }
+      if(local_co2 < min_co2){ //Liga os injetores
+        find_set_actuators(DEVICE_CO2_SENSOR_OR_INJECTOR, ACTUATOR_ON);
+      }
+      if(local_humidity > max_humidity){ //Desliga os irrigadores
+        find_set_actuators(DEVICE_SOIL_MOISTURE_OR_IRRIGATION, ACTUATOR_OFF);
+      }
+      if(local_humidity < min_humidity){ //Liga os irrigadores
+        find_set_actuators(DEVICE_SOIL_MOISTURE_OR_IRRIGATION, ACTUATOR_ON);
+      }
+
+      sleep(1);
+    }
   void wait_for_quit() {
     std::cout << "Press 'q' + Enter to shut down the manager.\n";
     char c;
@@ -237,6 +321,8 @@ public:
 int main(int argc, char *argv[]) {
   Manager manager = Manager();
 
+  manager.open_listening_socket();
+  std::thread(&Manager::control_loop, &manager).detach();
   manager.open_sockets();
 
   std::thread quit_thread(&Manager::wait_for_quit, &manager);
